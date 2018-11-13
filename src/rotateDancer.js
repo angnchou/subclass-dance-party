935c50fb8f0b9ea3eeeb65bbaf660bbc5bfa4e00
Angela Chou <angnchou@gmail.com>
2018-11-13 09:52:55 -0800

    add new dancer

---
+++ src/rotateDancer.js
@@ -0,0 +1,17 @@
+var makeRotateDancer = function(top, left, timeBetweenSteps) {
+  makeDancer.call(this, top, left, timeBetweenSteps);
+  this.oldStep = makeDancer.prototype.step;
+  this.step();
+  // this.$node = $('<img class="dancer" src="dancingBaby.gif"></img>');  
+};
+
+makeRotateDancer.prototype = Object.create(makeDancer.prototype);
+makeRotateDancer.prototype.constructor = makeRotateDancer;
+makeRotateDancer.prototype.step = function() {
+  // call the old version of step at the beginning of any call to this new version of step
+  this.oldStep(this.timeBetweenSteps);
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.animate();
+};
